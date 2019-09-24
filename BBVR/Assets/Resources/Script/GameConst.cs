792e6ad3b237a04d49ed954ac7734e4096b2f20b
nusashi <nusashi@gmail.com>
2019-09-25 02:42:37 +0900

    テストコード群を追加

---
+++ BBVR/Assets/Resources/Script/GameConst.cs
@@ -0,0 +1,14 @@
+﻿namespace Const
+{
+
+    public static class Const
+    {
+        public const int CONST_INT = 10;
+
+        public static readonly float[] CONST_LIST = {
+            1.0f, 2.0f, 3.0f
+        };
+
+    }
+
+}
---
+++ BBVR/Assets/Resources/Script/SceneManagerBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerBase : MonoBehaviour
+{
+
+    static int updateLine = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (updateLine)
+        {
+            case 0:
+                Initialize();
+                updateLine++;
+                break;
+            case 1:
+                MainLoop();
+                break;
+        }
+    }
+
+    public virtual void Initialize()
+    {
+        Debug.Log("Initialize");
+    }
+
+    public virtual void MainLoop()
+    {
+
+    }
+}
