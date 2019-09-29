fc46fb43b7756a84121d0f8f5c43ccfb3e275102
nusashi <nusashi@gmail.com>
2019-09-29 14:24:48 +0900

    跳ねないボールを追加

--- BBVR/Assets/Resources/Script/BallController.cs
+++ BBVR/Assets/Resources/Script/BallController.cs
@@ -11,13 +11,25 @@ public class BallController : MonoBehaviour
         // rigidbodyを取得
         Rigidbody rb = this.GetComponent<Rigidbody>();
         // 力を設定
-        Vector3 force = new Vector3(0.0f, 0.0f, -10.0f) * speed;
+        //float x = Random.Range(1, 10);
+        //float y = Random.Range(1, 10);
+        //Debug.Log("x =" + x);
+        //Debug.Log("y =" + y);
+        Vector3 force = new Vector3(1.0f, 0.0f, 1.0f) * speed;
         // 力を加える
         rb.AddForce(force);
+        //rb.AddForce(
+        //    (transform.forward + transform.right) * speed,
+        //    ForceMode.VelocityChange);
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
     {
         
     }
