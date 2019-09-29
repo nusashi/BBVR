1cf97a6f97638f732235ab44a6f2335a03ea9a57
nusashi <nusashi@gmail.com>
2019-09-29 10:34:50 +0900

    VR機能の追加

---
+++ BBVR/Assets/Editor/MyWindow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MyWindow : EditorWindow
+{
+    string myString = "Hello World";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    // Window メニューに "My Window" というメニュー項目を追加
+    [MenuItem("Window/My Window")]
+    public static void ShowWindow()
+    {
+        //既存のウィンドウのインスタンスを表示。ない場合は作成します。
+        EditorWindow.GetWindow(typeof(MyWindow));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        myString = EditorGUILayout.TextField("Text Field", myString);
+
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+        myBool = EditorGUILayout.Toggle("Toggle", myBool);
+        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+        EditorGUILayout.EndToggleGroup();
+    }
+}
